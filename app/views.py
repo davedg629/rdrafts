357691212a6a3064a7060400da35d2060cfefbb1
David DiGiovanni <davedg629@gmail.com>
2015-10-28 23:16:04 -0400

    Remove 'is' from views.py

--- app/views.py
+++ app/views.py
@@ -164,14 +164,14 @@ def preview(thread_id):
     thread = db.session.query(Thread)\
         .filter_by(id=thread_id)\
         .first()
-    if ((thread and thread.user_id == g.user.id) or (g.user.role_id is 1)) \
-            and thread.submitted is False:
+    if ((thread and thread.user_id == g.user.id) or (g.user.role_id == 1)) \
+            and thread.submitted == False:
         return render_template(
             'preview.html',
             thread=thread,
             page_title="Draft Preview"
         )
-    elif thread.submitted is True:
+    elif thread.submitted == True:
         return redirect(url_for(
             'success',
             thread_id=thread.id
@@ -186,7 +186,7 @@ def share(thread_id):
     thread = db.session.query(Thread)\
         .filter_by(id=thread_id)\
         .first()
-    if thread and thread.submitted is False:
+    if thread and thread.submitted == False:
         return render_template(
             'share.html',
             thread=thread,
@@ -204,8 +204,8 @@ def edit(thread_id):
         .filter_by(user_id=g.user.id)\
         .filter_by(id=thread_id)\
         .first()
-    if ((thread and thread.user_id == g.user.id) or (g.user.role_id is 1)) \
-            and thread.submitted is False:
+    if ((thread and thread.user_id == g.user.id) or (g.user.role_id == 1)) \
+            and thread.submitted == False:
         form = ThreadForm(obj=thread)
         if form.validate_on_submit():
             thread.title = form.title.data
@@ -221,7 +221,7 @@ def edit(thread_id):
             form=form,
             page_title="Edit Draft"
         )
-    elif thread.submitted is True:
+    elif thread.submitted == True:
         return redirect(url_for(
             'success',
             thread_id=thread.id
@@ -237,9 +237,9 @@ def success(thread_id):
     thread = db.session.query(Thread)\
         .filter_by(id=thread_id)\
         .first()
-    if thread and thread.user_id is g.user.id:
+    if thread and thread.user_id == g.user.id:
 
-        if thread.submitted is False:
+        if thread.submitted == False:
 
             # post to reddit
             reddit_post = None
@@ -326,7 +326,7 @@ def captcha(thread_id):
     thread = db.session.query(Thread)\
         .filter_by(id=thread_id)\
         .first()
-    if thread and thread.user_id is g.user.id and thread.submitted is False:
+    if thread and thread.user_id == g.user.id and thread.submitted == False:
         form = CaptchaForm()
         if form.validate_on_submit():
             return redirect(url_for(
